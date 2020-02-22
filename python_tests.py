5c981ee7650a467ad743ee3bc96f511ed0dbf606
Emīls Vizulis <esmils@inbox.lv>
2020-02-22 21:18:25 +0200

    20200222_21_17

---
+++ python_tests.py
@@ -0,0 +1,16 @@
+a=10
+
+import math
+print(math.cos(a))
+
+from math import cos
+print(cos(a))
+
+from math import cos as cos_V1
+print(cos_V1(a))
+
+from cmath impoer cos as cos_V2
+print(cos_V2(a))
+
+from math import *
+print(cos(a))
